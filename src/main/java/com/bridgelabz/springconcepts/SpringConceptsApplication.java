052c5f945d3bf22f8eb5fe447f485ea7941ebd4a
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-29 22:33:09 +0530

    ADDED: initialized project structure

---
+++ src/main/java/com/bridgelabz/springconcepts/SpringConceptsApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.springconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConceptsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Spring Concepts Demo");
+		SpringApplication.run(SpringConceptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/springconcepts/SpringConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
