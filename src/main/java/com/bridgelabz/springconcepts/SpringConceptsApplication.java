fe4dd770b0ad3c48dac01ad75bcc5be9461f48e0
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-30 00:35:53 +0530

    ADDED: demonstrated dependency injection using DepartmentBean and EmployeeBean

--- src/main/java/com/bridgelabz/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/bridgelabz/springconcepts/SpringConceptsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 import com.bridgelabz.springconcepts.component.DemoBean;
+import com.bridgelabz.springconcepts.component.EmployeeBean;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
@@ -17,6 +18,10 @@ public class SpringConceptsApplication {
 		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
 		DemoBean demoBean = context.getBean(DemoBean.class);
 		logger.debug("Demo Bean = "+ demoBean.toString());
+		EmployeeBean employeeBean = context.getBean(EmployeeBean.class);
+		employeeBean.setEid(104);
+		employeeBean.setEname("Spring Framework Guru");
+		employeeBean.showEmployeeDetails();
 	}
 
 }
