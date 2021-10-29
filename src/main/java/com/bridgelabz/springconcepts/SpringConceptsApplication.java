f409feb3712a4c81122a8447edd69e9e2df21689
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-29 23:37:58 +0530

    ADDED: implemented DemoBean

--- src/main/java/com/bridgelabz/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/bridgelabz/springconcepts/SpringConceptsApplication.java
@@ -2,13 +2,18 @@ package com.bridgelabz.springconcepts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.bridgelabz.springconcepts.component.DemoBean;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Spring Concepts Demo");
-		SpringApplication.run(SpringConceptsApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
+		DemoBean demoBean = context.getBean(DemoBean.class);
+		System.out.println("Demo Bean = "+ demoBean.toString());
 	}
 
 }
